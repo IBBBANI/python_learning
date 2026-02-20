7f67b58c02b79d9545fa8b3c5b626d3cbdbc975a
ibbani <ibbanigowda98@gmail.com>
2026-02-20 15:57:07 +0530

    type

--- type.py
+++ type.py
@@ -1,4 +1,8 @@
-x=["hello",3,4,5,True,"bye"]
-for i in x:
-    print(i)
-    print(type(i))
+def homogenous_list_hetrogenous_list(data):
+    ret_list = []
+    for val in data:
+        if type(val) != str:
+            ret_list.append(val)
+    return ret_list
+
+print(homogenous_list_hetrogenous_list(["hello",45.0,7,8,9,"appu","pothu","gubbi"]))
